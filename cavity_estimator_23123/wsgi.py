3230959510a5592643a6f8314ed8e3586a953d2c
Crowdbotics <team@crowdbotics.com>
2020-12-02 00:26:55 +0000

    Initial commit.

---
+++ cavity_estimator_23123/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cavity_estimator_23123 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cavity_estimator_23123.settings')
+
+application = get_wsgi_application()
